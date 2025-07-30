43cff7408e2d6b1c5711d5255a7950253256e76a
anubhavchk007 <anubhavclasher2262@gmail.com>
2025-07-30 20:06:24 +0530

    modified cors origin

--- src/main/java/com/practice/url_shortener_v2/config/WebConfig.java
+++ src/main/java/com/practice/url_shortener_v2/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://url-shortener-v2-frontend.vercel.app")
                 .allowedMethods("*");
     }
 }
